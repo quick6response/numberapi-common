192973cb86e805863d7dcc53b4caa406493746c0
Andrey Romanov <64421792+quick6response@users.noreply.github.com>
2024-05-09 00:08:39 +0300

    Изменено наименование интерфейса

---
+++ src/types/parameter.request.vk.type.ts
@@ -0,0 +1,19 @@
+/**
+ * Данные приходящие с сервиса АПИ после действия пользователя в системе в ВК
+ */
+export interface ParameterRequestVkUserEventInterface {
+  vk_access_token_settings: string;
+  vk_app_id: string;
+  vk_are_notifications_enabled: number;
+  vk_is_app_user: number;
+  vk_is_favorite: number;
+  vk_language: string;
+  vk_platform: string;
+  vk_ref: string;
+  vk_ts: string;
+  vk_user_id: number;
+  sign: string;
+  ip: string;
+  userAgent: string;
+  date: number;
+}
