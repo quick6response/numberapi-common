9b4d646831aec47eba8f540f8e5c98fcd69f08f1
Andrey Romanov <64421792+quick6response@users.noreply.github.com>
2024-05-11 00:56:43 +0300

    Добавлен тестовый тип для работы с телеграмм

--- src/microservice/notification/types/index.ts
+++ src/microservice/notification/types/index.ts
@@ -1,24 +1,24 @@
 import { ParameterRequestVkUserEventInterface } from '@microservice/notification';
 import { ClientPlatformEnum } from '@microservice/notification/types/parameter.request.action.type';
+import { ParameterRequestTelegramUserEventInterface } from '@microservice/notification/types/parameter.request.tg.type';
 
 export * from './parameter.request.vk.type';
 export * from './rabbitmq.number.api.service.type';
 
 // множество разных интерфейсов, которые написаны под разные платформы
 export type ParameterClientInfoActionEventAllPlatform =
-  ParameterRequestVkUserEventInterface;
+  | ParameterRequestVkUserEventInterface
+  | ParameterRequestTelegramUserEventInterface;
 
 // тип, который помогает узнать информацию о пользователе на основе типа платформы
 export type ParameterClientInfoActionEvent<
   Platform extends ClientPlatformEnum,
 > = Platform extends ClientPlatformEnum.VK
   ? ParameterRequestVkUserEventInterface
-  : //   : Platform extends ClientPlatformEnum.VK_BOT
-    //     ? // Добавьте здесь тип для VK_BOT
-    // : Platform extends ClientPlatformEnum.TELEGRAM
-    //   ? // Добавьте здесь тип для TELEGRAM
-    //   : Platform extends ClientPlatformEnum.ODNOKLASSNIKI
-    //   ? // Добавьте здесь тип для ODNOKLASSNIKI
-    //   : Platform extends ClientPlatformEnum.WEB
-    //   ? // Добавьте здесь тип для WEB
-    never;
+  : Platform extends ClientPlatformEnum.TELEGRAM
+    ? ParameterRequestTelegramUserEventInterface
+    : //   : Platform extends ClientPlatformEnum.ODNOKLASSNIKI
+      //   ? // Добавьте здесь тип для ODNOKLASSNIKI
+      //   : Platform extends ClientPlatformEnum.WEB
+      //   ? // Добавьте здесь тип для WEB
+      never;
---
+++ src/microservice/notification/types/parameter.request.tg.type.ts
@@ -0,0 +1,21 @@
+import {
+  ClientPlatformEnum,
+  ParameterRequestActionInterface,
+} from '@microservice/notification/types/parameter.request.action.type';
+
+interface ClientInfoTg {
+  tg_user_id: number;
+  ip: string;
+  userAgent: string;
+}
+
+/**
+ * Данные приходящие с сервиса АПИ после действия пользователя в системе в ВК
+ */
+export interface ParameterRequestTelegramUserEventInterface
+  extends ParameterRequestActionInterface<
+    ClientInfoTg,
+    ClientPlatformEnum.TELEGRAM
+  > {
+  // информация о пользователе из строки запуска и дополнительные параметры
+}
