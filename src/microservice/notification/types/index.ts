a070105b5327b0833cbc185e1a9039da5eb11fea
Andrey Romanov <64421792+quick6response@users.noreply.github.com>
2024-08-18 15:39:29 +0300

    Улучшены экспорты

--- src/microservice/notification/types/index.ts
+++ src/microservice/notification/types/index.ts
@@ -1,2 +1,4 @@
 export * from './parameter.client.info.type';
 export * from './rabbitmq.events.data.type';
+export * from './parameter.request.vk.type';
+export * from './parameter.request.tg.type';
