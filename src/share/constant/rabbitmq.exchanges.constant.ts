feb3fad7c581b87a0194cfac4ecb1f624bce24ca
Andrey Romanov <64421792+quick6response@users.noreply.github.com>
2024-12-22 23:49:05 +0300

    Добавлен новый тип отправителя

--- src/share/constant/rabbitmq.exchanges.constant.ts
+++ src/share/constant/rabbitmq.exchanges.constant.ts
@@ -1,6 +1,11 @@
+/**
+ * Отправители событий
+ */
 export const RabbitmqExchangesConstant = {
   // микросервис уведомлений проекта
   notification: 'vk_notification_exchanges',
   // микросервис главного АПИ проекта
   mainServiceApi: 'service_api_exchanges',
+  // оплата подписки вк донут
+  vkPayment: 'vk_payment_exchanges',
 } as const;
